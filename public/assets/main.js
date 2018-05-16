1d157213ebd6e381b4ae9148736944cfe90e7943
James Badger <james@jamesbadger.ca>
2018-05-15 23:21:39 -0600

    Add sample station data

--- public/assets/main.js
+++ public/assets/main.js
@@ -2,4 +2,13 @@ $(function() {
   console.log("Ready.");
   var map = polarMap("map-view");
 
+  var stationGroup = L.layerGroup();
+  map.addLayer(stationGroup);
+
+  // Load Stations
+  $.getJSON("/assets/sample-data.json", function(data) {
+    data["stations"].forEach(function(element) {
+      L.geoJson(element["location"]).addTo(stationGroup);
+    });
+  });
 });
