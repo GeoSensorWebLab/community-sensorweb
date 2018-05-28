22d500ea270f2bcb9a082bcfc547513d69a0e6ac
James Badger <james@jamesbadger.ca>
2018-05-28 17:06:24 -0600

    Populate live stats with almost real data

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -58,7 +58,52 @@ export default Controller.extend({
           "name": "°C",
           "symbol": ""
         },
-        "observationType": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Observation"
+        "observationType": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Observation",
+        // Hack to give a shortcut to latest Observation
+        "LastObservation": {
+          "@iot.id": 102669,
+          "@iot.selfLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102669)",
+          "phenomenonTime": "2018-05-28T21:00:00.000Z",
+          "result": "-3.4",
+          "Datastream@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102669)/Datastream",
+          "FeatureOfInterest@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102669)/FeatureOfInterest",
+          "resultTime": "2018-05-28T21:04:24.877Z"
+        },
+        "Observations": [
+          {
+            "@iot.id": 102669,
+            "@iot.selfLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102669)",
+            "phenomenonTime": "2018-05-28T21:00:00.000Z",
+            "result": "-3.4",
+            "Datastream@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102669)/Datastream",
+            "FeatureOfInterest@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102669)/FeatureOfInterest",
+            "resultTime": "2018-05-28T21:04:24.877Z"
+          }, {
+            "@iot.id": 102240,
+            "@iot.selfLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102240)",
+            "phenomenonTime": "2018-05-28T20:00:00.000Z",
+            "result": "-0.2",
+            "Datastream@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102240)/Datastream",
+            "FeatureOfInterest@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(102240)/FeatureOfInterest",
+            "resultTime": "2018-05-28T20:03:45.534Z"
+          }, {
+            "@iot.id": 101809,
+            "@iot.selfLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(101809)",
+            "phenomenonTime": "2018-05-28T19:00:00.000Z",
+            "result": "1.6",
+            "Datastream@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(101809)/Datastream",
+            "FeatureOfInterest@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(101809)/FeatureOfInterest",
+            "resultTime": "2018-05-28T19:01:29.040Z"
+          }, {
+            "@iot.id": 101371,
+            "@iot.selfLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(101371)",
+            "phenomenonTime": "2018-05-28T18:00:00.000Z",
+            "result": "2.8",
+            "Datastream@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(101371)/Datastream",
+            "FeatureOfInterest@iot.navigationLink": "https://sensors.arcticconnect.org:6443/v1.0/Observations(101371)/FeatureOfInterest",
+            "resultTime": "2018-05-28T18:02:36.107Z"
+          }
+        ]
       }]
     }
   ]),
