ac68bb5c25dcdacbeae8781558c750db70fa669f
James Badger <james@jamesbadger.ca>
2018-06-06 13:35:14 -0600

    Move generic code to superclass

--- app/serializers/location.js
+++ app/serializers/location.js
@@ -1,37 +1,3 @@
-import DS from 'ember-data';
+import EntitySerializer from './entity';
 
-export default DS.JSONSerializer.extend({
-  primaryKey: '@iot.id',
-
-  // Customize method to handle payload.value array
-  normalizeResponse(store, primaryModelClass, payload, id, requestType, isSingle) {
-    let documentHash = {
-      data: null
-    };
-
-    let meta = this.extractMeta(store, primaryModelClass, payload);
-    if (meta) {
-      assert(
-        'The `meta` returned from `extractMeta` has to be an object, not "' + typeOf(meta) + '".',
-        typeOf(meta) === 'object'
-      );
-      documentHash.meta = meta;
-    }
-
-    if (isSingle) {
-      let { data } = this.normalize(primaryModelClass, payload);
-      documentHash.data = data;
-    } else {
-      let ret = new Array(payload.value.length);
-      for (let i = 0, l = payload.value.length; i < l; i++) {
-        let item = payload.value[i];
-        let { data } = this.normalize(primaryModelClass, item);
-        ret[i] = data;
-      }
-
-      documentHash.data = ret;
-    }
-
-    return documentHash;
-  }
-});
+export default EntitySerializer.extend({});
