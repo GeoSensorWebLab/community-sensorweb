0f1a9986120822f3b6debff26bfb1e9a335235f1
James Badger <james@jamesbadger.ca>
2019-10-01 14:02:30 -0600

    Rebuild Serializer based on JSON:API Serializer
    This is the start of switching to a serialization class that will
    handle translating SensorThings API responses into valid JSON:API
    for use by Ember Data.
    
    This first commit will parse collection responses from SensorThings
    API. It maps the entities into data objects, with attributes, links,
    and relationships.
    
    Still to do:
    
    * Handle getting a single entity from STA as a response
    * Use the `@iot.count` attribute for meta collection information
    * Move entities included using `$expand` into the `included` array
      used in JSON:API
    * Refactor the Serializer class to separate concerns for methods,
      which will be used as the Serializer is expanded to handle more
      cases
    
    (The reverse case of serializing JSON:API data for sending data to
    SensorThings API is not planned at this time, but should be doable
    with the Serializer class. JSON:API seems close enough to OData to
    work.)

--- app/serializers/entity.js
+++ app/serializers/entity.js
@@ -1,187 +1,15 @@
-import { assert, warn } from '@ember/debug';
-import { typeOf, isNone } from '@ember/utils';
 import DS from 'ember-data';
+import { isNone, typeOf } from '@ember/utils';
 
-export default DS.JSONSerializer.extend({
-  primaryKey: '@iot.id',
-
-  coerceId(id) {
-    if (id === null || id === undefined || id === '') { return null; }
-    if (typeof id === 'string') { return id; }
-    return '' + id;
-  },
-
+export default DS.JSONAPISerializer.extend({
   /**
-    Extract any SensorThings API links from the payload into a JSON-API
-    compatible links object.
-    
-    @method extractLinks
-    @param {Object} payload
-    @return {Object} links
-  */
-  extractLinks(payload) {
-    if (payload['@iot.nextLink']) {
-      return {
-        next: payload['@iot.nextLink']
-      };
-    }
-  },
-
-  /**
-    `extractMeta` is used to deserialize any meta information in the
-    adapter payload. By default Ember Data expects meta information to
-    be located on the `meta` property of the payload object.
-    
-    This method has been overridden for SensorThings API. 
-
-    @method extractMeta
-    @param {DS.Store} store
-    @param {DS.Model} modelClass
-    @param {Object} payload
-  */
-  extractMeta(store, typeClass, payload) {
-    if (payload && payload['@iot.count'] !== undefined) {
-      return { count: payload['@iot.count'] };
-    }
-  },
-
-  /**
-    Returns a relationship formatted as a JSON-API "relationship 
-    object".
-
-    http://jsonapi.org/format/#document-resource-object-relationships
-
-    This method has been overridden for SensorThings API.
-
-    @method extractRelationship
-    @param {Object} relationshipModelName
-    @param {Object} relationshipHash
-    @return {Object}
-  */
-  extractRelationships(modelClass, resourceHash) {
-    let relationships = {};
-
-    modelClass.eachRelationship((key, relationshipMeta) => {
-      let relationship = null;
-
-      let linkKey = this.keyForLink(key, relationshipMeta.kind);
-      if (resourceHash[linkKey] !== undefined) {
-        let related = resourceHash[linkKey];
-        relationship = relationship || {};
-        relationship.links = { related };
-      }
-
-      if (relationship) {
-        relationships[key] = relationship;
-      }
-    });
-
-    return relationships;
-  },
-
-  /**
-   `keyForLink` can be used to define a custom key when deserializing 
-   link properties.
-
-   This method has been overridden for SensorThings API.
-
-   @method keyForLink
-   @param {String} key
-   @param {String} kind `belongsTo` or `hasMany`
-   @return {String} normalized key
-  */
-  keyForLink(key, kind) {
-    const keys = {
-      'datastream':       'Datastream',
-      'datastreams':      'Datastreams',
-      'locations':        'Locations',
-      'observations':     'Observations',
-      'observedProperty': 'ObservedProperty',
-      'thing':            'Thing',
-      'things':           'Things'
-    }
-
-    let properKey = keys[key];
-    if (properKey === undefined) {
-      console.warn('Unhandled keyForLink', key);
-    }
-
-    return `${properKey}@iot.navigationLink`;
-  },
-
-  /**
-    `keyForRelationship` can be used to define a custom key when
-    serializing and deserializing relationship properties. By default
-    `JSONSerializer` does not provide an implementation of this method.
-    
-    This method has been overridden for SensorThings API to convert the
-    case of relationships in Ember Data to match the case in SensorThings
-    API entities.
-
-    @method keyForRelationship
-    @param {String} key
-    @param {String} typeClass
-    @param {String} method
-    @return {String} normalized key
-  */
-  keyForRelationship(key, typeClass, method) {
-    const keys = {
-      'datastream':       'Datastream',
-      'datastreams':      'Datastreams',
-      'locations':        'Locations',
-      'observations':     'Observations',
-      'observedProperty': 'ObservedProperty',
-      'thing':            'Thing',
-      'things':           'Things'
-    }
-
-    let properKey = keys[key];
-    if (properKey === undefined) {
-      console.warn('Unhandled keyForLink', key);
-    }
-
-    return properKey;
-  },
-
-   /**
-    In SensorThings API, a request may be paginated server-side. The 
-    Ember Adapter is set up to retrieve the pages recursively up to an
-    optionally specified limit, and return an array of responses to the
-    Ember Store, which passes that to here as the payload.
-
-    An array of responses must be reduced to a single payload object
-    that can be normalized using the generic normalizer.
-
-    This method has been overridden for SensorThings API.
-
-    @method normalizeArrayResponse
-    @param {DS.Store} store
-    @param {DS.Model} primaryModelClass
-    @param {Object} payload
-    @param {String|Number} id
-    @param {String} requestType
-    @return {Object} JSON-API Document
-  */
-  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
-    return payload.reduce((newPayload, aPayload) => {
-      let normalized = this.normalizeResponseGeneric(store, primaryModelClass, aPayload, id, requestType, false);
-      newPayload.data = newPayload.data.concat(normalized.data);
-      return newPayload;
-    }, {
-      data: []
-    });
-  },
-
-  /**
-    The `normalizeResponse` method is used to normalize a payload from the
-    server to a JSON-API Document.
+    The `normalizeResponse` method is used to normalize a payload from
+    OGC SensorThings API to a JSON:API Document.
 
     http://jsonapi.org/format/#document-structure
-
+    
     This method delegates to a more specific normalize method based on
     the `requestType`.
-
-    This method has been overridden for SensorThings API.
 
     @method normalizeResponse
     @param {DS.Store} store
@@ -189,140 +17,79 @@ export default DS.JSONSerializer.extend({
     @param {Object} payload
     @param {String|Number} id
     @param {String} requestType
-    @return {Object} JSON-API Document
+    @return {Object} JSON:API Document
   */
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    switch (requestType) {
-      case 'findRecord':
-        // This is a single record
-        return this.normalizeResponseGeneric(...arguments, true);
-      case 'queryRecord':
-        return this.normalizeResponseGeneric(...arguments);
-      case 'findAll':
-        return this.normalizeFindAllResponse(...arguments);
-      case 'findBelongsTo':
-        return this.normalizeResponseGeneric(...arguments);
-      case 'findHasMany':
-        return this.normalizeFindHasManyResponse(...arguments);
-      case 'findMany':
-        return this.normalizeResponseGeneric(...arguments);
-      case 'query':
-        return this.normalizeQueryResponse(...arguments);
-      case 'createRecord':
-        return this.normalizeResponseGeneric(...arguments);
-      case 'deleteRecord':
-        return this.normalizeResponseGeneric(...arguments);
-      case 'updateRecord':
-        return this.normalizeResponseGeneric(...arguments);
-    }
-  },
-
-  /**
-    Normalize a findAll response. As this is a SensorThings API entity
-    collection, it is an array of one or more responses.
-
-    This method has been overridden for SensorThings API.
-
-    @method normalizeQueryResponse
-    @param {DS.Store} store
-    @param {DS.Model} primaryModelClass
-    @param {Object} payload
-    @param {String|Number} id
-    @param {String} requestType
-    @return {Object} JSON-API Document
-  */
-  normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
-    return this.normalizeArrayResponse(...arguments);
-  },
-
-  /**
-    Normalize a findHasMany response. As this is a SensorThings API 
-    entity collection, it is an array of one or more responses.
-
-    This method has been overridden for SensorThings API.
-
-    @method normalizeFindHasManyResponse
-    @param {DS.Store} store
-    @param {DS.Model} primaryModelClass
-    @param {Object} payload
-    @param {String|Number} id
-    @param {String} requestType
-    @return {Object} JSON-API Document
-  */
-  normalizeFindHasManyResponse(store, primaryModelClass, payload, id, requestType) {
-    return this.normalizeArrayResponse(...arguments);
-  },
-
-  /**
-    Normalize a query response. As this is a SensorThings API entity
-    collection, it is an array of one or more responses.
-
-    This method has been overridden for SensorThings API.
-
-    @method normalizeQueryResponse
-    @param {DS.Store} store
-    @param {DS.Model} primaryModelClass
-    @param {Object} payload
-    @param {String|Number} id
-    @param {String} requestType
-    @return {Object} JSON-API Document
-  */
-  normalizeQueryResponse(store, primaryModelClass, payload, id, requestType) {
-    return this.normalizeArrayResponse(...arguments);
-  },
-
-   /*
-    Method based on DS.RestSerializer's generic response normalizer.
-
-    This method has been re-defined for SensorThings API.
-
-    @method normalizeResponseGeneric
-    @private
-    @param {DS.Store} store
-    @param {DS.Model} primaryModelClass
-    @param {Object} payload
-    @param {String|Number} id
-    @param {String} requestType
-    @param {Boolean} isSingle
-    @return {Object} JSON-API Document
-  */
-  normalizeResponseGeneric(store, primaryModelClass, payload, id, requestType, isSingle) {
     let documentHash = {
-      data: null
+      data: null,
+      included: []
     };
 
-    let meta = this.extractMeta(store, primaryModelClass, payload);
-    if (meta) {
-      assert(
-        'The `meta` returned from `extractMeta` has to be an object, not "' + typeof(meta) + '".',
-        typeof(meta) === 'object'
-      );
-      documentHash.meta = meta;
-    }
-
-    let links = this.extractLinks(payload);
-    if (links) {
-      assert(
-        'The `links` returned from `extractLinks` has to be an object, not "' + typeof(links) + '".',
-        typeof(links) === 'object'
-      );
-      documentHash.links = links;
-    }
-
-    if (isSingle || requestType === "findBelongsTo") {
-      let { data } = this.normalize(primaryModelClass, payload);
-      documentHash.data = data;
+    // If payload is an array, parse each set of responses one at a time
+    if (typeOf(payload) !== "array") {
+      console.error("Unhandled payload type")
     } else {
-      let ret = new Array(payload.value.length);
-      for (let i = 0, l = payload.value.length; i < l; i++) {
-        let item = payload.value[i];
-        let { data } = this.normalize(primaryModelClass, item);
-        ret[i] = data;
-      }
+      // Initialize empty array instead of null value
+      documentHash.data = [];
 
-      documentHash.data = ret;
+      payload.forEach((response) => {
+        // TODO: Extract meta information such as the collection size
+        // from the STA response
+
+        // If the response is a single entity
+        if (typeOf(response.value) !== "array") {
+
+        } else {
+          // If the response is a collection of entities
+          let entities = response.value;
+
+          entities.forEach((entity) => {
+            // Convert entity for JSON:API `data` array
+            // TODO: Extract entities retrieved with $expand to the 
+            //       JSON:API `included` array
+            
+            let dataEntity = {
+              id: entity["@iot.id"],
+              type: primaryModelClass.modelName,
+              attributes: entity,
+              relationships: {},
+              links: {}
+            };
+
+            // Remove links from attributes
+            Object.keys(dataEntity.attributes).forEach((key) => {
+              let value = dataEntity.attributes[key];
+
+              // For links to related entities
+              if (key.includes("@iot.navigationLink")) {
+                // Get the name of the relationship from the key.
+                // TODO: Find the correct case and pluralization for
+                // mapping between STA and JSON:API and Ember Data
+                let relationshipName = key.split("@")[0];
+
+                // We use `related` instead of `self` as it is not a
+                // SensorThings API `@iot.selfLink`.
+                dataEntity.relationships[relationshipName] = {
+                  links: { related: value }
+                };
+                delete dataEntity.attributes[key];
+                
+              } else if (key.includes("@iot.selfLink")) {
+                // Copy the self link to the links object
+                dataEntity.links.self = value;
+                delete dataEntity.attributes[key];
+              }
+            });
+
+            // Remove id from attributes
+            delete dataEntity.attributes["@iot.id"];
+
+            documentHash.data.push(dataEntity);
+          });
+        }
+      });
     }
 
     return documentHash;
-  }
+  },
 });
