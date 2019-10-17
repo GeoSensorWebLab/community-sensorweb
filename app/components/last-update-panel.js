063988dc3cc2b050f21e79a718f610c0a963664c
James Badger <james@jamesbadger.ca>
2019-10-17 10:52:37 -0600

    Move "Last Update" to its own component
    Component may also be clicked to switch between "Time since last
    update" and "Date string of last update". That is useful for
    debugging.

---
+++ app/components/last-update-panel.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+/*
+ * Panel to display the time of the latest Observation for the station
+ * that is visible in the live panels
+ */
+export default Component.extend({
+  classNames: ['panel'],
+
+  station: null,
+  displayDateOffset: true,
+
+  actions: {
+    switchDate() {
+      this.set('displayDateOffset', !this.get('displayDateOffset'));
+    }
+  }
+});
