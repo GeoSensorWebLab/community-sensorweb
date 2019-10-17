850e582c53cd953638b86587f1b164e8e6650d14
James Badger <james@jamesbadger.ca>
2019-10-17 11:24:10 -0600

    Improve formatting of "Last Update" date
    Uses browser locale formatting on the ISO8601 date to make it more
    readable. Additionally, component helpers "is-pending" and
    "is-fulfilled" are used to avoid passing promises to the formatting
    helper.

---
+++ app/helpers/format-date.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+import { helper } from '@ember/component/helper';
+
+/*
+  Convert an ISO8601 Date String (which may contain fractional seconds)
+  to a human-readable Date string in the user's locale.
+ */
+export function formatDate([proxy]) {
+  let date = new Date(proxy.content);
+  return date.toLocaleString();
+}
+
+export default helper(formatDate);
