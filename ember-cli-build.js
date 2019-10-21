4717af0134262eb41e08223718ca679a3256a908
James Badger <james@jamesbadger.ca>
2019-10-21 15:12:53 -0600

    Remove ember-leaflet library
    Leaflet is being replaced with OpenLayers for better projection
    support. OpenLayers 6 also supports modular loading for more
    efficient bandwidth usage. Removal of Ember-leaflet will also allow
    for upgrades to Ember and Ember Data, as there is a bug in octane
    that is preventing the upgrade.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,11 +7,6 @@ module.exports = function(defaults) {
     // Add options here
     fingerprint: {
       exclude: [
-        'images/layers-2x.png',
-        'images/layers.png',
-        'images/marker-icon-2x.png',
-        'images/marker-icon.png',
-        'images/marker-shadow.png'
       ]
     }
   });
