bd5727e40de87957d201325c2f003ea12649cf00
James Badger <james@jamesbadger.ca>
2019-10-22 11:55:08 -0600

    Add options for TileLayer
    These are read from the template or can be set on the Component.

--- app/components/openlayers-map/tile-layer.js
+++ app/components/openlayers-map/tile-layer.js
@@ -3,26 +3,39 @@ import Component from '@ember/component';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 /*
- * Sub-component for a layer on an interactive web map
+ * Sub-component for a layer on an interactive web map.
+ * See the OL docs for more details:
+ * https://openlayers.org/en/latest/apidoc/module-ol_layer_Tile-TileLayer.html
  */
 export default Component.extend({ 
   parentComponent: null,
 
-  label: null,
-  // The tile loading URL for this layer.
-  url: null,
   // The attribution string for this layer. Will be displayed when this
   // layer is active.
   attribution: null,
+  // Bounding extent for the layer, which will not be rendered outside
+  // this extent. Use `ol/extent` object.
+  extent: null,
+  opacity: 1.0,
+  // Projection of the tiles, defaults to Web Mercator (EPSG:3857).
+  projection: 'EPSG:3857',
+  // The URL for the XYZ source for this layer.
+  url: null,
+  visible: true,
+
 
   init() {
     this._super(...arguments);
     this.get('parentComponent').on('mapReady', (map) => {
       let tileLayer = new TileLayer({
+        extent: this.get('extent'),
+        opacity: this.get('opacity'),
         source: new XYZ({
           attributions: this.get('attribution'),
+          projection: this.get('projection'),
           url: this.get('url')
-        })
+        }),
+        visible: this.get('visible')
       });
 
       map.addLayer(tileLayer);
