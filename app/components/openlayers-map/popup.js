1225a19d54b2e1e722b943f6494ec55228ea931a
James Badger <james@jamesbadger.ca>
2019-10-25 16:44:11 -0600

    Display multiple options in feature popup
    If multiple features are close to the point selected, display a
    popup that lets the user select an option from a list. This works
    even when multiple features are at the same exact location.
    
    If there are more than a dozen options, it may look bad in the popup
    and another solution may be needed.

--- app/components/openlayers-map/popup.js
+++ app/components/openlayers-map/popup.js
@@ -13,31 +13,37 @@ export default Component.extend({
   // Title for the feature, to be displayed in a popup
   title: null,
 
+  // If there are multiple features, then a different popup is rendered
+  // to let the user choose between features
+  multipleFeatures: null,
+
   init() {
     this._super(...arguments);
     this.get('parentComponent').on('ready', (map) => {
       let popup = new Overlay({
         element: this.$()[0],
         positioning: 'bottom-center',
-        stopEvent: false,
+        stopEvent: true,
         offset: [0, -10]
       });
       map.addOverlay(popup);
 
       // Set up click handler on the map to activate the "onClick" 
       // handler for the feature closest to the click.
+      // Note that setting properties on the components will cause the
+      // popup to draw, so don't set properties if there are no features
+      // to draw.
       map.on('click', (event) => {
         let features = map.getFeaturesAtPixel(event.pixel);
+        // convert to feature objects for the template
+        let featureObjects = features.map(feature => feature.getProperties());
 
-        // TODO: If multiple features are near the click event, a popup
-        // should be used to select one
-        if (features.length > 1) {
-          console.warn("Not yet supported");
-        } else if (features.length == 1) {
+        if (features.length > 0) {
+          this.set('features', featureObjects);
+          this.set('multipleFeatures', (features.length > 1));
+
           let feature = features[0];
           let coordinates = feature.getGeometry().getCoordinates();
-
-          this.set('title', feature.get('title'));
 
           // Set the popup center coordinates *after* Ember renders 
           // the updated popup contents, otherwise the popup won't be
@@ -45,9 +51,11 @@ export default Component.extend({
           this.one('didRender', () => {
             popup.setPosition(coordinates);
           });
+        }
 
-          // Activate the onClick callback for the feature component
-          let onClick = feature.get('onClick');
+        // Activate the onClick callback for the feature component
+        if (features.length === 1) {
+          let onClick = features[0].get('onClick');
           if (onClick) {
             onClick();
           }
